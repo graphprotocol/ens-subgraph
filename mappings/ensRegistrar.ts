8164019c75b9de3e920fb0a799b47d445efbba11
David Kajpust <kajpustd@gmail.com>
2018-09-26 08:41:39 -0400

    updates to the mappings

--- mappings/ensRegistrar.ts
+++ mappings/ensRegistrar.ts
@@ -1,28 +1,91 @@
-export function transfer(event: Transfer): void {
-  let domain = new Entity()
+export function newOwner(event: NewOwner): void {
+  let ensDomain = new Entity()
+  // here we will actually have to calculate the subdomain namehash, since the event emitted is just label and node, and not the resultant subnode 
+  // let subnode = namehash.subdomain(events.params.node + events.params.label)
 
-  domain.setAddress('ownerAddr', event.params.owner)
-  domain.setString('domainHash', event.params.node.toHex())
+  //temporary fix until I can get namehash algorithm to work in type script
+  // let id = (event.params.node + "-" + event.params.label).toHex()
 
-  store.set('Domain', event.params.node.toHex(), domain)
+  // ensDomain.setString('id', id)
+  ensDomain.setAddress('owner', event.params.owner)
+  ensDomain.setString('node', event.params.node.toHex())
+  ensDomain.setString('label', event.params.label.toHex())
 
+  store.set('EnsDomain', event.params.node.toHex(), ensDomain)
 }
 
-export function newOwner(event: NewOwner): void {
-  let subdomain = new Entity()
+export function transfer(event: Transfer): void {
+  let ensDomain = new Entity()
+  let id = event.params.node.toHex()
 
-    // let auctionID = Auction.bind(event.address, event.blockHash).uniqueID()
+  ensDomain.setString('id', id)
+  ensDomain.setAddress('owner', event.params.owner)
 
-  subdomain.setAddress('ownerAddr', event.params.owner)
-  subdomain.setString('domainHash', event.params.node.toHex())
-  subdomain.setString('subDomainHash', event.params.label.toHex())
+  store.set('EnsDomain', id, ensDomain)
+}
 
 
-  //might neeed to hex the id tuype 
-  //ADDED THE CAPITAL D, will this fix? 
-  store.set('SubDomain', event.params.node.toHex(), subdomain)
+// const namehash = require('eth-ens-namehash')
+
+
+// export function transfer(event: Transfer): void {
+//   let domain = new Entity()
+
+//   domain.setAddress('ownerAddr', event.params.owner)
+//   domain.setString('domainHash', event.params.node.toHex())
+
+//   store.set('Domain', event.params.node.toHex(), domain)
+
+// }
+
+// export function newOwner(event: NewOwner): void {
+//   let subdomain = new Entity()
+
+
+//   //here we will actually have to calculate the name hash, since the event emitted is just label and node, and not the resultant subnode 
+//   // let subnode = namehash.hash(events.params.node + events.params.label)
+
+
+
+//   subdomain.setAddress('ownerAddr', event.params.owner)
+//   subdomain.setString('parentNode', event.params.node.toHex())
+//   subdomain.setString('label', event.params.label.toHex())
+
+
+//   //might neeed to hex the id tuype 
+//   //ADDED THE CAPITAL D, will this fix? 
+//   store.set('SubDomain', event.params.node.toHex(), subdomain)
 
   
-}
+// }
 
+//will un comment soon 
+
+// export function transfer(event: Transfer): void {
+//   let ensDomain = new Entity()
+//   let id = event.params.node.toHex()
+
+//   ensDomain.setString('id', id)
+//   ensDomain.setAddress('owner', event.params.owner)
+
+//   store.set('EnsDomain', id, ensDomain)
+// }
+
+// export function newOwner(event: NewOwner): void {
+//   let ensDomain = new Entity()
+
+
+//   // here we will actually have to calculate the subdomain namehash, since the event emitted is just label and node, and not the resultant subnode 
+//   let subnode = namehash.subdomain(events.params.node + events.params.label)
+
+//   let id = subnode.toHex()
+
+//   ensDomain.setString('id', id)
+//   ensDomain.setAddress('owner', event.params.owner)
+
+//   ensDomain.setString('node', event.params.node.toHex())
+//   ensDomain.setString('label', event.params.label.toHex())
+
+//   store.set('EnsDomain', id, ensDomain)
+// }
 
--- scripts/namehash.js
+++ scripts/namehash.js
@@ -1,4 +1,5 @@
 var namehash = require('eth-ens-namehash')
+var web3 = require("web3")
 // Also supports normalizing strings to ENS compatibility:
 // var input = getUserInput()
 // var normalized = namehash.normalize(input)
@@ -8,7 +9,7 @@ var string2 = 'mywallet.eth'
 var string3 = 'richard.mywallet.eth'
 var string4 = 'richard'
 var string5 = 'david.thisisme.eth'
-var string6 = 'mywallet'
+var string6 = 'eth'
 var string7 = 'mywallet'
 var string8 = 'mywallet'
 var string9 = 'mywallet'
@@ -19,11 +20,22 @@ var hash2 = namehash.hash(string2)
 var hash3 = namehash.hash(string3)
 var hash4 = namehash.hash(string4)
 var hash5 = namehash.hash(string5)
-var hash6 = namehash.hash(string6)
-var hash7 = namehash.hash(string7)
-var hash8 = namehash.hash(string8)
-var hash9 = namehash.hash(string9)
-var hash10 = namehash.hash(string10)
+// var hash6 = namehash.hash(string6)
+// var hash7 = namehash.hash(string7)
+// var hash8 = namehash.hash(string8)
+// var hash9 = namehash.hash(string9)
+// var hash10 = namehash.hash(string10)
+
+//This returns sha3('richard') without the 0x prefix
+var labelHash4 = (web3.utils.soliditySha3(string4)).substring(2)
+
+//This returns the namehash('mywallet.eth') without the 0x prfix
+var remove0x = hash2.substring(2)
+
+// var buildFromLabelAndNode = web3.utils.soliditySha3(remove0x + labelHash4)
+// var hardcide = web3.utils.soliditySha3("200a4ff549fd75c2b282f412e84fdaf57a1b1d45e21ea2b551951c871dff12b7" + "4f5b812789fc606be1b3b16908db13fc7a9adf7ca72641f84d75b47069d3d7f0" )
+
+var subdomain4 =  namehash.subdomain(remove0x, labelHash4)
 
 console.log("\n")
 console.log(string1 + " : " + hash1 + "\n")
@@ -32,6 +44,12 @@ console.log(string3 + " : " + hash3 + "\n")
 console.log(string4 + " : " + hash4 + "\n")
 console.log(string5 + " : " + hash5 + "\n")
 
+console.log("\n")
+console.log("Label Hash 4: " + labelHash4 )
+console.log("remove 0x and get hash of mywallet.eth: " + remove0x)
+// console.log(buildFromLabelAndNode)
+// console.log("HARCOED: " + hardcide)
 
+console.log("richard.mywallet.eth subdomain: " + subdomain4)
 
 
